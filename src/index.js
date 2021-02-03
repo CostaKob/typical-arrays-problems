9befaedc2811b075e91d1955c58255d7a9d5646c
Costa <costakob@gmail.com>
2021-02-03 20:11:01 +0200

    solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  result = 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < result) {
+      result = array[i]
+    }
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  result = 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > result) {
+      result = array[i]
+    }
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let sum = array.reduce((acc, el) => acc + el);
+  return sum / array.length
 }
